309948a762a19d5bb20cdf8897754a695765d40f
matheeshagit <12matheesha@gmail.com>
2026-03-20 09:19:11 +0530

    Implement JWTService to generate token

--- back_end/src/main/java/smart_campus/back_end/auth/service/JWTService.java
+++ back_end/src/main/java/smart_campus/back_end/auth/service/JWTService.java
@@ -1,4 +1,32 @@
 package smart_campus.back_end.auth.service;
 
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Service;
+import smart_campus.back_end.auth.model.User;
+
+import java.util.Date;
+
+@Service
 public class JWTService {
+    private String SECRET_KEY = "my-secret-key";
+
+    public String generateToken(User user){
+        return Jwts.builder()
+                .setSubject(user.getEmail())
+                .claim("role", user.getRoles())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 86400000))
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+
+    public String extractEmail(String token){
+        return Jwts.parser()
+                .setSigningKey(SECRET_KEY)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
 }
