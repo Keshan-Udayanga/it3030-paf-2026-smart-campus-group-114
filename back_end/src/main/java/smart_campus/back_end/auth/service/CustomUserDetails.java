1741c6604ed28e447cc6d67e2c00b6c7aa2656ff
matheeshagit <12matheesha@gmail.com>
2026-03-20 10:07:49 +0530

    Implement CustomUserDetails as Security representation

---
+++ back_end/src/main/java/smart_campus/back_end/auth/service/CustomUserDetails.java
@@ -0,0 +1,56 @@
+package smart_campus.back_end.auth.service;
+
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import smart_campus.back_end.auth.model.User;
+
+
+import java.util.Collection;
+import java.util.List;
+
+public class CustomUserDetails implements UserDetails {
+    private User user;
+
+    public CustomUserDetails(User user){
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return  user.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role.toUpperCase()))
+                .toList();
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+}
---
+++ back_end/src/main/java/smart_campus/back_end/auth/service/JWTAuthenticationFilter.java
@@ -0,0 +1,4 @@
+package smart_campus.back_end.auth.service;
+
+public class JWTAuthenticationFilter {
+}
